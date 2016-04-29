6f9db661d88b73550ed30aa9ec7a87a3fec0e578
vandosant <sjskender@gmail.com>
2016-04-29 15:22:36 -0600

    Refactoring to use es2015

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
-var express = require('express');
-var app = express();
-var jsonData = {count: 12, message: 'hey'};
+const express = require('express');
+const app = express();
+const jsonData = {count: 12, message: 'hey'};
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html', function(err) {
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`, function(err) {
     if (err) {
       res.status(500);
       res.send(err);
@@ -14,8 +14,8 @@ app.get('/', function(req, res) {
   });
 });
 
-app.get('/data', function(req, res) {
+app.get('/data', (req, res) => {
   res.json(jsonData);
 });
 
-app.listen(8000, function() { console.log('listening on http://localhost:8000') });
+app.listen(8000, () => console.log('listening on http://localhost:8000') );
